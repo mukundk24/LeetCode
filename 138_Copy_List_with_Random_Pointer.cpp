856262b4513e89d03c6c4d0f6172f4a2cf603794
Mukund Kansara <mukundkansara2411@gmail.com>
2025-01-03 12:02:53 +0530

    Add Another approach for Copy List with Random Pointer

--- 138_Copy_List_with_Random_Pointer.cpp
+++ 138_Copy_List_with_Random_Pointer.cpp
@@ -43,3 +43,73 @@ public:
         return clonehead;
     }
 };
+
+// Another Approach without using map
+class Solution {
+private:
+void InsertAtTail(Node*& head, Node*& tail, int data)
+{
+    Node* newnode = new Node(data);
+    if(head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    tail->next = newnode;
+    tail = newnode;
+}
+public:
+    Node* copyRandomList(Node* head) {
+        Node* clonehead = NULL;
+        Node* clonetail = NULL;
+        Node *temp = head;
+        while(temp!=NULL)
+        {
+            InsertAtTail(clonehead, clonetail, temp->val);
+            temp = temp->next;
+        }
+        Node* orgnode = head;
+        Node* clnode = clonehead;
+        while(orgnode!=NULL && clnode!=NULL)
+        {
+            Node* nnext = orgnode->next;
+            orgnode->next = clnode;
+            orgnode = nnext;
+
+            Node* cnnext = clnode->next;
+            clnode->next = orgnode;
+            clnode = cnnext;
+        }
+
+        temp = head;
+        while(temp!=NULL)
+        {
+            if(temp->next!=NULL)
+            {
+                if(temp->random != NULL)
+                {
+                temp->next->random = temp->random->next;
+                }
+                else
+                {
+                    temp->next->random = temp->random;
+                }
+            }
+            temp = temp->next->next;
+        }
+        orgnode = head;
+        clnode = clonehead;
+        while(orgnode!=NULL && clnode!=NULL)
+        {
+            orgnode->next = clnode->next;
+            orgnode = orgnode->next;
+            if(orgnode!=NULL)
+            {
+            clnode->next = orgnode->next;
+            }
+            clnode = clnode->next;
+        }
+        return clonehead;
+    }
+};
